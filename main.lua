160bead761e58ac53c464f84292974cd0cb367f8
Karp <54753268+Karp666@users.noreply.github.com>
2025-01-04 06:07:31 +0200

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,40 @@
+function GetPluginAuthor()
+    return "karp"
+end
+function GetPluginVersion()
+    return "v1.0.0"
+end
+function GetPluginName()
+    return "bonus"
+end
+function GetPluginWebsite()
+    return ""
+end
+commands:Register("bonus", function (playerid, args)
+    local player = GetPlayer(playerid)
+    local credits = tonumber(args[1])
+    
+    if not exports["admins"]:HasFlags(playerid, config:Fetch("shop.core.adminflag")) then
+        return ReplyToCommand(playerid, "{default}[{yellow}Haos{default}]", " {darkred}Nu ai acces la aceasta comanda!")
+    end
+
+    if not credits then
+        return ReplyToCommand(playerid, "{default}[{yellow}Haos{default}]", "{green}Syntax: {default}<credite>")
+        end
+
+for playerid = 0,playermanager:GetPlayerCap() -1 do
+    if not player or not player:IsValid() or player:IsFakeClient() then
+        return end
+        
+        exports["shop-core"]:GiveCredits(playerid, credits)
+    
+    ::CONTINUE::
+    
+    end
+
+    local targetPlayer = nil
+    if playerid ~= -1 then
+        targetPlayer = GetPlayer(playerid)
+    end
+    playermanager:SendMsg(MessageType.Chat, FetchTranslation("bonus.bonus_all"):gsub("{CREDITS}", credits):gsub("{PLAYER_NAME}", targetPlayer:CBasePlayerController().PlayerName))
+end)
