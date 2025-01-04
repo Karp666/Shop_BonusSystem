17994ba12f4b90d0a88479cc02ba8314d42d8c85
Karp <54753268+Karp666@users.noreply.github.com>
2025-01-04 06:28:26 +0200

    Fix for loop

--- main.lua
+++ main.lua
@@ -11,27 +11,25 @@ function GetPluginWebsite()
     return ""
 end
 commands:Register("bonus", function (playerid, args)
-    local player = GetPlayer(playerid)
-    local credits = tonumber(args[1])
-    
-    if not exports["admins"]:HasFlags(playerid, config:Fetch("shop.core.adminflag")) then
-        return ReplyToCommand(playerid, "{default}[{yellow}Haos{default}]", " {darkred}Nu ai acces la aceasta comanda!")
-    end
+local player = GetPlayer(playerid)
+local credits = tonumber(args[1])
 
-    if not credits then
-        return ReplyToCommand(playerid, "{default}[{yellow}Haos{default}]", "{green}Syntax: {default}<credite>")
-        end
+if not exports["admins"]:HasFlags(playerid, config:Fetch("shop.core.adminflag")) then
+    return ReplyToCommand(playerid, "{default}[{yellow}Haos{default}]", " {darkred}Nu ai acces la aceasta comanda!")
+end
+
+if not credits then
+    return ReplyToCommand(playerid, "{default}[{yellow}Haos{default}]", "{green}Syntax: {default}<credite>")
+    end
 
 for playerid = 0,playermanager:GetPlayerCap() -1 do
     if not player or not player:IsValid() or player:IsFakeClient() then
-        return end
-        
-        exports["shop-core"]:GiveCredits(playerid, credits)
-    
-    ::CONTINUE::
-    
+        goto continue
     end
+    exports["shop-core"]:GiveCredits(playerid, credits)
+    ::continue::
 
+end
     local targetPlayer = nil
     if playerid ~= -1 then
         targetPlayer = GetPlayer(playerid)
